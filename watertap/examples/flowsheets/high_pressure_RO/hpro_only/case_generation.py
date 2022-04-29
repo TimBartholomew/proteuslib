3e526c1edf59cfc571284f9c1c330a0ab9bb2484
TimBartholomew <timothy.bartholomew@netl.doe.gov>
2022-04-28 21:04:43 -0400

    ran black

--- watertap/examples/flowsheets/high_pressure_RO/hpro_only/case_generation.py
+++ watertap/examples/flowsheets/high_pressure_RO/hpro_only/case_generation.py
@@ -5,13 +5,13 @@ import time
 from idaes.core.util import get_solver
 from pyomo.environ import Expression, Param, Constraint
 from watertap.tools.parameter_sweep import _init_mpi, LinearSample, parameter_sweep
-import watertap.examples.flowsheets.high_pressure_RO.hpro_only.hpro as high_pressure_RO
+import watertap.examples.flowsheets.high_pressure_RO.hpro_only.flowsheet as high_pressure_RO
 
 
 def run_analysis(case, nx, interp_nan_outputs=False):
     sweep_params = {}
     outputs = {}
-    optimize_kwargs = {'check_termination': False}
+    optimize_kwargs = {"check_termination": False}
 
     # build, set, and initialize
     m = high_pressure_RO.build(case=case)
@@ -24,29 +24,33 @@ def run_analysis(case, nx, interp_nan_outputs=False):
     high_pressure_RO.optimize(m)
 
     # set up parameter sweep
-    if case == 'seawater':
+    if case == "seawater":
         lb = 0.3
         ub = 0.85
     else:
         lb = 0.3
         ub = 0.95
-    sweep_params['System Recovery'] = LinearSample(m.fs.product_recovery, lb, ub, nx)
+    sweep_params["System Recovery"] = LinearSample(m.fs.product_recovery, lb, ub, nx)
 
     for j in m.fs.disposal.properties[0].conc_mass_comp:
         outputs[j] = m.fs.disposal.properties[0].conc_mass_comp[j]
 
-    outputs['LCOW'] = m.fs.costing.LCOW
+    outputs["LCOW"] = m.fs.costing.LCOW
 
-    output_filename = 'output/oli_cases/'+case+'.csv'
+    output_filename = "output/oli_cases/" + case + ".csv"
 
     opt_function = high_pressure_RO.optimize
 
-
-    global_results = parameter_sweep(m, sweep_params, outputs, csv_results_file=output_filename,
-                                     optimize_function=opt_function,
-                                     optimize_kwargs=optimize_kwargs,
-                                     debugging_data_dir=os.path.split(output_filename)[0]+'/local',
-                                     interpolate_nan_outputs=interp_nan_outputs)
+    global_results = parameter_sweep(
+        m,
+        sweep_params,
+        outputs,
+        csv_results_file=output_filename,
+        optimize_function=opt_function,
+        optimize_kwargs=optimize_kwargs,
+        debugging_data_dir=os.path.split(output_filename)[0] + "/local",
+        interpolate_nan_outputs=interp_nan_outputs,
+    )
 
     return global_results, sweep_params
 
@@ -59,7 +63,7 @@ if __name__ == "__main__":
     # analysis
     # case = 'seawater'
     # case = 'brackish_1'
-    case = 'brackish_2'
+    case = "brackish_2"
     nx = 30
 
     tic = time.time()
@@ -73,6 +77,9 @@ if __name__ == "__main__":
         for k, v in sweep_params.items():
             total_samples *= v.num_samples
 
-        print('Finished case = %s.' % case)
-        print('Processed %d swept parameters comprising %d total points.' % (len(sweep_params), total_samples))
-        print('Elapsed time = %.1f s.' % (toc-tic))
+        print("Finished case = %s." % case)
+        print(
+            "Processed %d swept parameters comprising %d total points."
+            % (len(sweep_params), total_samples)
+        )
+        print("Elapsed time = %.1f s." % (toc - tic))
---
+++ watertap/examples/flowsheets/high_pressure_RO/hpro_only/flowsheet_oli.py
@@ -0,0 +1,97 @@
+###############################################################################
+# WaterTAP Copyright (c) 2021, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National
+# Laboratory, National Renewable Energy Laboratory, and National Energy
+# Technology Laboratory (subject to receipt of any required approvals from
+# the U.S. Dept. of Energy). All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#
+###############################################################################
+from pyomo.environ import (
+    ConcreteModel,
+    Constraint,
+    Expression,
+    Param,
+    Var,
+    units as pyunits,
+    assert_optimal_termination,
+)
+from idaes.core import FlowsheetBlock
+from idaes.core.util.model_statistics import degrees_of_freedom
+from idaes.generic_models.unit_models.mixer import MomentumMixingType
+import idaes.core.util.scaling as iscale
+import idaes.logger as idaeslog
+
+import watertap.examples.flowsheets.high_pressure_RO.hpro_only.flowsheet as high_pressure_RO
+
+from idaes.surrogate.pysmo import polynomial_regression, radial_basis_function
+
+
+def main(case="seawater", surrogate_type="polynomial"):
+    m = high_pressure_RO.build(case=case)
+    high_pressure_RO.specify_model(m)
+    high_pressure_RO.initialize_model(m)
+
+    # simulate and display
+    high_pressure_RO.solve(m)
+    print("\n***---Simulation results---***")
+    high_pressure_RO.display_results(m)
+
+    # optimize and display
+    high_pressure_RO.set_up_optimization(m)
+    high_pressure_RO.optimize(m)
+    print("\n***---Optimization results---***")
+    high_pressure_RO.display_results(m)
+
+    print("\n***---Optimization results with scaling surrogate---***")
+    # add scaling surrogate
+    add_scaling_surrogate(m, surrogate_type=surrogate_type)
+    m.fs.product_recovery.unfix()
+    m.fs.gypsum_scaling_index.fix(1)
+    high_pressure_RO.optimize(m)
+    high_pressure_RO.display_results(m)
+    print("---scaling index---")
+    print("Gypsum scaling index: %.2f" % m.fs.gypsum_scaling_index.value)
+
+
+def add_scaling_surrogate(m, surrogate_type="polynomial"):
+    dir_path = r"C:\Users\timvb\Box\WaterTAP (protected by NDA)\nawi (NDA protected)\WaterTAP-OLI (protected data)\Workspace\feed_cases\\"
+    if surrogate_type == "polynomial":
+        file_name = m.case + "_gypsum_surrogate.pickle"
+        gypsum_scaling_index_surrogate = (
+            polynomial_regression.PolynomialRegression.pickle_load(dir_path + file_name)
+        )
+    elif surrogate_type == "rbf":
+        file_name = m.case + "_gypsum_surrogate_interpolating.pickle"
+        gypsum_scaling_index_surrogate = (
+            radial_basis_function.RadialBasisFunctions.pickle_load(dir_path + file_name)
+        )
+
+    m.fs.gypsum_scaling_index = Var(
+        initialize=1, bounds=(0, None), units=pyunits.dimensionless
+    )
+
+    m.fs.product_recovery_indexed = Var(
+        [0], initialize=0.73, bounds=(0, 1), units=pyunits.dimensionless
+    )
+    m.fs.eq_product_recovery_indexed = Constraint(
+        expr=m.fs.product_recovery == m.fs.product_recovery_indexed[0]
+    )
+
+    m.fs.eq_gypsum_scaling_index = Constraint(
+        expr=(
+            m.fs.gypsum_scaling_index
+            == gypsum_scaling_index_surrogate["model"].generate_expression(
+                [m.fs.product_recovery_indexed[0]]
+            )
+        )
+    )
+
+
+if __name__ == "__main__":
+    case_list = ["seawater", "brackish_1", "brackish_2"]
+    surrogate_type_list = ["polynomial", "rbf"]
+    main(case=case_list[0], surrogate_type=surrogate_type_list[1])
